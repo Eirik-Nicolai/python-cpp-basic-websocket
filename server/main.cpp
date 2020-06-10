1d2b884cf2653359f8bd2eea9fd5f2c4a88f48dd
Eirik-Nicolai <eirik.n.o.oseberg@gmail.com>
2020-06-10 21:19:04 +0200

    Set up boost for C++ and atom, finally

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <boost/random.hpp>
+#include <boost/generator_iterator.hpp>
+using namespace std;
+
 #include "main.h"
 
 int main()
 {
-    std::cout << add(LARGE_NUMBER, 2) << std::endl;
+  typedef boost::mt19937 RNGType;
+      RNGType rng;
+      rng.seed(static_cast<unsigned int>(std::time(0)));
+      boost::uniform_int<> one_to_six( 1, 6 );
+      boost::variate_generator< RNGType, boost::uniform_int<> >
+                    dice(rng, one_to_six);
+      for ( int i = 0; i < 6; i++ ) {
+          int n  = dice();
+          cout << n << endl;
+     }
 }
 
 int add(const int & a, const int & b)
